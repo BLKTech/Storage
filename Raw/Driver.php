3e11dd4f6469c3ae1b6a9b01fcaab686b4ab0c77
blktech <github@blktech.com>
2019-01-23 05:55:56 +0000

    Auto Commit mié ene 23 05:55:56 UTC 2019

---
+++ Raw/Driver.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace BLKTech\Storage\Raw;
+
+/**
+ *
+ */
+abstract class Driver
+{
+    public abstract function exists($id);
+    public abstract function delete($id);
+    public abstract function get($id);
+    public abstract function set($id,$data);
+    public abstract function spaceAvailable();  
+    public abstract function getUID();
+}

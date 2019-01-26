a98e75e265b0a27d41424bb9ce64bc454206a640
blktech <github@blktech.com>
2019-01-26 22:15:17 +0000

    Auto Commit sáb ene 26 22:15:17 UTC 2019

--- String/Driver/SQL/Dynamic.php
+++ String/Driver/SQL/Dynamic.php
@@ -15,7 +15,7 @@
 
 namespace BLKTech\Storage\String\Driver\SQL;
 use BLKTech\DataBase\SQL\Driver\MySQL;
-
+use BLKTech\DataType\Integer;
 /**
  *
  * @author TheKito < blankitoracing@gmail.com >
@@ -30,34 +30,61 @@ class Dynamic extends \BLKTech\Storage\String\Driver\SQL
         $this->driver = $driver;
     }
 
-    public function delete($id) {
-        
+    public function delete($id) 
+    {
+        $_ = $this->getTableAndWhere($id);
+        return $this->driver->delete($_['table'], $_['where']);        
     }
 
-    public function exists($id) {
-        
+    public function exists($id) 
+    {
+        $_ = $this->getTableAndWhere($id);
+        return $this->driver->exists($_['table'], $_['where']);
     }
 
     public function get($id) {
-        
+        $_ = $this->getTableAndWhere($id);
+        return $this->driver->getText($_['table'], 'value', $_['where']);
     }
 
     public function set($string) 
     {
         $length = mb_strlen($string);
         $table = $this->getTableNameString($length);
-        $this->driver->
-                
-                
+        $id = array_pop($this->driver->autoTable($table, array('value'=>$string), array('id')));
+        
+        return Integer::unSignedInt32CombineIntoInt64($length, $id);
     }
 
-    
+    private function getTableAndWhere($id)
+    {
+        $_ = Integer::unSignedInt64UnCombineIntoInt32($id);                
+        return array(
+            'table'=>$this->getTableNameString(mb_strlen($_[0])),
+            'where'=>array('id'=>$_[1])
+        );
+    }
+    private function checkTable($tableName)
+    {
+        static $_ = null;
+        
+        if($_ === null)
+            $_ = array();
+        
+        if(isset($_[$tableName]))
+            return false;
+        else
+        {
+            $_[$tableName] = true;
+            return true;
+        }
+    }     
     private function getTableNameString($length)
     {
         $_ = 'blktech_storage_string__' . $length;
         
         if($this->checkTable($_))
-            $this->driver->command("CREATE TABLE IF NOT EXISTS `" . $_ . "` (`id` int(11) NOT NULL AUTO_INCREMENT, `value` char(" . $length . ") NOT NULL, PRIMARY KEY (id),UNIQUE (`value`)) ENGINE=MyISAM;");
+            $this->driver->command("CREATE TABLE IF NOT EXISTS `" . $_ . "` (`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, `value` char(" . $length . ") NOT NULL, PRIMARY KEY (id),UNIQUE (`value`)) ENGINE=MyISAM;");
         
         return $_;        
     }    

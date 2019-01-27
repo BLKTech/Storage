d9207c9fd7a8f31bdf9c23ea0a0ed1621e7c61fc
blktech <github@blktech.com>
2019-01-27 18:54:07 +0000

    Auto Commit dom ene 27 18:54:07 UTC 2019

--- Link/Driver/DataBase/SQL/Dynamic.php
+++ Link/Driver/DataBase/SQL/Dynamic.php
@@ -8,6 +8,9 @@
 
 namespace BLKTech\Storage\Link\Driver\DataBase\SQL;
 use BLKTech\DataType\Integer;
+use BLKTech\DataBase\SQL\Driver\MySQL;
+use BLKTech\DataBase\SQL\Driver\MySQL\Dynamic;
+
 
 /**
  * Description of Dynamic
@@ -19,31 +22,60 @@ class Dynamic extends \BLKTech\Storage\Link\Driver\DataBase\SQL{
     const tableNamePrefix='blktech_storage_link__';
     
     private $driver;    
+    private $dynamic;    
     public function __construct(MySQL $driver)
     {
         $this->driver = $driver;
+        $this->dynamic = new Dynamic($driver);
     }    
     
     public function delete($id) 
     {
-        $id_ = Integer::unSignedInt64UnCombineIntoInt32($id);
+        return $this->dynamic->delete($this->tableNamePrefix,$id);
     }
 
     public function exists($id) 
     {
-        $id_ = Integer::unSignedInt64UnCombineIntoInt32($id);
+        return $this->dynamic->exists($this->tableNamePrefix,$id);
     }
 
     public function get($id) 
     {
         $id_ = Integer::unSignedInt64UnCombineIntoInt32($id);
-
+        $row = $this->dynamic->get($this->tableNamePrefix,$id);
+        return array(
+            Integer::unSignedInt32CombineIntoInt64($id_[0], $row['idSourceLow']),
+            Integer::unSignedInt32CombineIntoInt64($row['idDestinationHigh'], $row['idDestinationLow']),
+        );
     }
 
     public function set($idSource, $idDestination) 
     {
         $idSource_ = Integer::unSignedInt64UnCombineIntoInt32($idSource);
         $idDestination_ = Integer::unSignedInt64UnCombineIntoInt32($idDestination);
+        
+        $data = array(
+                'idSourceLow' => $idSource_[1],
+                'idDestinationHigh' => $idDestination_[0],
+                'idDestinationLow' => $idDestination_[1]
+            );
+                
+        $this->createTable($idSource_[0]);
+        return $this->dynamic->set($this->tableNamePrefix, $idSource_[0], $data);
     }
-
+   
+    private function createTable($suffix)
+    {
+        $tableName = self::tableNamePrefix . $suffix;
+                
+        static $_ = null;
+        
+        if($_ === null)
+            $_ = array();
+        
+        if(!isset($_[$tableName]))        
+            $_[$tableName] = $this->driver->command("CREATE TABLE IF NOT EXISTS `" . $tableName . "` (`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, `idSourceLow` int(11) UNSIGNED NOT NULL, `idDestinationHigh` int(11) UNSIGNED NOT NULL, `idDestinationLow` int(11) UNSIGNED NOT NULL, PRIMARY KEY (id),INDEX (`idSourceLow`)) ENGINE=MyISAM;");
+        
+        return $tableName;        
+    }    
 }
